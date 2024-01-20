b1c8ad5374fd4db89f3477f21a8fa1787859bb39
neel-icp <131347438+neel-icp@users.noreply.github.com>
2024-01-21 05:03:21 +0530

    - deploy fix

--- my-landing-page/src/Hero.js
+++ my-landing-page/src/Hero.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Hero.css';
-import backgroundImg from './herobg.png'; // Update with the correct path
+
 import secondaryImg from './hero.png'; // Update with the correct path
 
 function Hero() {
