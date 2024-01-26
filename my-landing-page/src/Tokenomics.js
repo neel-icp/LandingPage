8f20702678ab60565eed7ebebb81d064adc15e1c
neel-icp <131347438+neel-icp@users.noreply.github.com>
2024-01-26 22:16:08 +0530

    contract

--- my-landing-page/src/Tokenomics.js
+++ my-landing-page/src/Tokenomics.js
@@ -5,6 +5,7 @@ function Tokenomics() {
       <section id="about">
         <h2>MIAnomics</h2>
         <p>🔥 100% LIQUIDITY & TEAM TOKENS BURNED 🔥 <br/> MINT AUTHORITY DISABLED <br/>TOTAL SUPPLY : 800 MILLION $ MIA </p>
+        <p><span className="contract-address">Contract Address<br/> Abam1vB9kSXEGA9N7pPifuXwe2xVuqetWBDQDrrvQ43e</span></p>
       </section>
   );
 }
