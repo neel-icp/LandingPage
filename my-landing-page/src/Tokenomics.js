f6f71d36c0acdc2a95213555246aaf1ebdd8b9b3
neel-icp <131347438+neel-icp@users.noreply.github.com>
2024-03-16 04:48:01 +0530

    CleanUp

--- my-landing-page/src/Tokenomics.js
+++ my-landing-page/src/Tokenomics.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Tokenomics.css'; // Adjust the path as needed
 
 function Tokenomics() {
   return (
       <section id="about">
         <h2>MIAnomics</h2>
         <p>🔥 100% LIQUIDITY & TEAM TOKENS BURNED 🔥 <br/> MINT AUTHORITY DISABLED <br/>TOTAL SUPPLY : 800 MILLION $ MIA </p>
-        <p><span className="contract-address">Contract Address<br/> Abam1vB9kSXEGA9N7pPifuXwe2xVuqetWBDQDrrvQ43e</span></p>
+        <p><span className="contract-address">Contract Address<br/>Abam1vB9kSXEGA9N7pPifuXwe2xVuqetWBDQDrrvQ43e</span></p>
       </section>
   );
 }
